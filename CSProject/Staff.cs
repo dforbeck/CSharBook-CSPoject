a88fdea9b1c412b55375f00bf0501a3705e241ae
Debbie <debbieforbeck@gmail.com>
2020-03-09 13:22:06 -0400

    beginning

---
+++ CSProject/Staff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProject
+{
+    class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        public float TotalPay {get; protected set;}
+        public float BasicPay {get; private set;}
+        public string NameOfStaff{get; private set;}
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0) hWorked = value;
+                else hWorked = 0;
+            }
+        }
+    }
+}

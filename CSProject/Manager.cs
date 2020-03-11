320dc453527ff8a467e25cc491cf335067690192
Debbie <debbieforbeck@gmail.com>
2020-03-11 12:26:57 -0400

    Manager class

--- CSProject/Manager.cs
+++ CSProject/Manager.cs
@@ -12,9 +12,27 @@ namespace CSProject
 
         public int Allowance { get; private set; }
 
-        public Manager(string name): base (name,managerHourlyRate)
+        public Manager(string name): base (name,managerHourlyRate)  //passing name and rate to base class
         {
-
         }
-    }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay(); //call method in Staff base
+
+            Allowance = 1000;
+
+            if (base.HoursWorked > 160) TotalPay += 1000;
+            else TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Name of Staff: " + NameOfStaff +
+                ", Manager Hourly Rate: " + managerHourlyRate +
+                ", Hours Worked: " + Allowance+
+                ", Total Pay: " + TotalPay;
+        }
+    }            
 }
